46278eddf98af725b197c2a1b63178ee4c691195
Jovan <belomarkovicdev@gmail.com>
2021-04-17 22:51:24 +0200

    Dispecer: prikaz i izmena podataka o taksi sluzbi

--- src/Model/Dispecer.java
+++ src/Model/Dispecer.java
@@ -14,11 +14,13 @@ public class Dispecer extends Korisnik {
     public Dispecer() {
     }
 
-    public Dispecer(double plata, String brojTelefonskeLinije, String odeljenjeRada) {
+    public Dispecer(long JMBG, String korisnickoIme, String lozinka, String ime, String prezime, String adresa, String pol, String brojTelefona, double plata, String brojTelefonskeLinije, String odeljenjeRada) {
+        super(JMBG, korisnickoIme, lozinka, ime, prezime, adresa, pol, brojTelefona);
         this.plata = plata;
         this.brojTelefonskeLinije = brojTelefonskeLinije;
         this.odeljenjeRada = odeljenjeRada;
     }
+
 
     public double getPlata() {
         return plata;
